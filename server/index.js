39f91f8764fbf1a40d01014df5079ba808989396
root <root@ixWorkLaptop.localdomain>
2025-09-20 17:12:53 -0700

    added solution to index.js

--- server/index.js
+++ server/index.js
@@ -1,32 +1,27 @@
-import express  from 'express'
-import cors from 'cors'
-import {restaurants} from './data.js'
+import express from "express";
+import cors from "cors";
+import { restaurants } from "./data.js";
 const app = express();
 const port = 3000;
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-
-
-app.get('/restaurants', (req, res) => {
+app.get("/restaurants", (req, res) => {
   res.json(restaurants);
 });
 
+app.get("/restaurants/:id", (req, res) => {
+  const restaurantId = parseInt(req.params.id, 10);
+  //Finds the restraunt using the .find method, .filter would work here too.
+  // .find is similar to .filter but it stops at the first resource that matches the condition
+  const restaurant = restaurants.find((r) => r.id === restaurantId);
 
-app.post('/restaurants', (req, res) => {
-  const newRestaurant = {
-    id: restaurants.length + 1,
-    name: req.body.name,
-    address: req.body.address,
-    phone: req.body.phone,
-    cuisine: req.body.cuisine,
-    rating: req.body.rating,
-    hours:req.body.hours,
-    menu: req.body.menu
-  };
-
-  restaurants.push(newRestaurant);
-  res.status(201).json(newRestaurant);
+  //If the resource doesn't exsist we want to send a 404 status code to our user
+  if (restaurant) {
+    res.json(restaurant);
+  } else {
+    res.status(404).json({ error: "Resource not found" });
+  }
 });
 
 app.listen(port, () => {
